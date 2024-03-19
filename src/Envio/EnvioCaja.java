f839d75e5be8b2f9e7cabbef06f971103372ca70
Joao Pedro Lima Dias (ext) <joao.lima.external@eviden.com>
2024-03-19 20:23:51 +0000

    Creando caja

--- src/Envio/EnvioCaja.java
+++ src/Envio/EnvioCaja.java
@@ -1,5 +1,8 @@
 package Envio;
 
-public class EnvioCaja {
-
+public class EnvioCaja implements Envio{
+	@Override
+	public void enviar(String mensaje) {
+		System.out.println("Caja enviada a la direccion: " + mensaje);
+	}
 }
